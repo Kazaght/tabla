5b07ce11670505193c190a32c7810ebca21464dd
Kazaght <92064007+Kazaght@users.noreply.github.com>
2021-10-30 19:40:16 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let tabla:string=document.getElementById
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+function tabladel( numerodetabla :number) {
+ let Rango: number ;
+  Rango=0;
+  for (let Rango = 1; Rango >=9 ; Rango++) {
+    let multiplicacion : number= Rango * numerodetabla
+if (tabla.value===A|| tabla.value=== B|| tabla.value=== C|| tabla.value=== D) {
+  tabladel(9)  
+} else
+ {
+  console.log ("Invalido. Ingrese los valores A, B, C o D ")
+  
+} 
